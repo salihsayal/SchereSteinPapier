d9887ca6f9f9893168665f6cd51a909b8842a63a
Muhammet Salih Sayal <muhammet.salih.sayal@hotmail.com>
2023-09-12 22:11:23 +0200

    Added getHandmove and createPlayer method

--- src/main/java/player/ChoosingRandomPlayer.java
+++ src/main/java/player/ChoosingRandomPlayer.java
@@ -1,6 +1,16 @@
 package player;
 
+import handmoves.Handmoves;
+
 //This player chooses random moves
 public class ChoosingRandomPlayer implements Player{
+    private Handmoves move;
 
+    public Handmoves getHandmove(){
+        return move;
+    }
+
+    public Player createPlayer(){
+        return new ChoosingRandomPlayer();
+    }
 }
