2a0624a69cdeb97d0a81745afec5cc19aea08a65
Muhammet Salih Sayal <muhammet.salih.sayal@hotmail.com>
2023-09-12 23:23:43 +0200

    getHandmove() now returns a random value of Handmoves

--- src/main/java/player/ChoosingRandomPlayer.java
+++ src/main/java/player/ChoosingRandomPlayer.java
@@ -2,15 +2,34 @@ package player;
 
 import handmoves.Handmoves;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //This player chooses random moves
 public class ChoosingRandomPlayer implements Player{
     private Handmoves move;
 
     public Handmoves getHandmove(){
-        return move;
+        return randomMove();
     }
 
-    public Player createPlayer(){
-        return new ChoosingRandomPlayer();
+    private Handmoves randomMove(){
+        List<Handmoves> allHandmoves = getAllHandmoves();
+        return allHandmoves.get(getRandomNumber(Handmoves.values().length, 0));
     }
+
+     //Creates a list with all Handmoves
+    private List<Handmoves> getAllHandmoves(){
+        List<Handmoves> moves = new ArrayList<>();
+        for(Handmoves move: Handmoves.values()){
+            moves.add(move);
+        }
+        return moves;
+    }
+
+    private int getRandomNumber(int max, int min){
+        return (int) (Math.random() * (max)+min);
+    }
+
+
 }
