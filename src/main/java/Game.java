17e5fd8b611924ca0adb8f14a04dec09bf763a71
Muhammet Salih Sayal <muhammet.salih.sayal@hotmail.com>
2023-09-12 21:53:05 +0200

    added constructor and getMethods

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,25 @@
+import player.Player;
+
+import java.util.List;
+
 public class Game {
-    public static void main(String[] args) {
-        
+    List<Player> playerList;
+    int numberOfRounds;
+    private Game(List<Player> playerList, int numberOfRounds){
+        this.playerList = playerList;
+        this.numberOfRounds = numberOfRounds;
     }
+
+    public List<Player> getPlayerList(){
+        return this.playerList;
+    }
+
+    public int getNumberOfRounds(){
+        return this.numberOfRounds;
+    }
+
+    public static Game createGame(List<Player> playerList, int numberOfRounds){
+        return new Game(playerList, numberOfRounds);
+    }
+
 }
