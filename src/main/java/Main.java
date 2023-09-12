229fbb5694bce4188fa6431e4b65d0861d94835f
Muhammet Salih Sayal <muhammet.salih.sayal@hotmail.com>
2023-09-12 23:25:23 +0200

    creates game with 2 players

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import player.AlwaysPaperPlayer;
+import player.ChoosingRandomPlayer;
+import player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+    Player playerA = new AlwaysPaperPlayer();
+    Player playerB = new ChoosingRandomPlayer();
+    List<Player> playerList = new ArrayList<>(List.of(playerA, playerB));
+
+    Game game = Game.createGame(playerList, 100);
+
+
+
+
+
+
+
 
     }
 }
