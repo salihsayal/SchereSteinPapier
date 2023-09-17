72b0c1790b475dc07c2b2e64c98a2785ac46e31e
Muhammet Salih Sayal <muhammet.salih.sayal@hotmail.com>
2023-09-17 19:32:41 +0200

    Splitted PlayerTest into AlwaysPaperPlayerTest and ChoosingRandomPlayerTest

---
+++ src/test/java/player/AlwaysPaperPlayerTest.java
@@ -0,0 +1,20 @@
+package player;
+
+import handmoves.Handmoves;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AlwaysPaperPlayerTest {
+
+    @Test
+    @DisplayName("Check, if AlwaysPaperPlayer's move is always Paper")
+    void AlwaysPaperPlayerTest_getHandmoveTest(){
+        Player player = new AlwaysPaperPlayer();
+        Handmoves move = player.getHandmove();
+        assertThat(move).isEqualTo(Handmoves.PAPER);
+
+    }
+
+}
--- src/test/java/player/ChoosingRandomPlayerTest.java
+++ src/test/java/player/ChoosingRandomPlayerTest.java
@@ -6,17 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class PlayerTest {
-
-    @Test
-    @DisplayName("Check, if AlwaysPaperPlayer's move is always Paper")
-    void AlwaysPaperPlayerTest_getHandmoveTest(){
-        Player player = new AlwaysPaperPlayer();
-        Handmoves move = player.getHandmove();
-        assertThat(move).isEqualTo(Handmoves.PAPER);
-
-    }
-
+class ChoosingRandomPlayerTest {
     @Test
     @DisplayName("Check, if ChoosingRandomPlayer is choosing randomly Paper")
     void ChoosingRandomPlayerTest_getHandmovePaperTest(){
@@ -71,5 +61,4 @@ class PlayerTest {
         assertThat(contains).isTrue();
 
     }
-
 }
