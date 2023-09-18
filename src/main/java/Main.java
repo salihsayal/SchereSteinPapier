909f0424c2e92e00b4dc722d6408344c0571b5c1
Muhammet Salih Sayal <muhammet.salih.sayal@hotmail.com>
2023-09-18 10:44:01 +0200

    n\a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
 
-        Player playerA = new AlwaysScissorsPlayer();
+        Player playerA = new AlwaysPaperPlayer();
         Player playerB = new ChoosingRandomPlayer();
         List<Player> playerList = new ArrayList<>(List.of(playerA, playerB));
 
